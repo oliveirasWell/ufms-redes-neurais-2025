2084aa1963a41a8abc7d86c45b2ab941de132695
Wellington Oliveira <well.oliveira.snt@gmail.com>
2025-11-02 00:18:35 -0400

    mobilenet_v2 - test 2

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 source venv/bin/activate
-jupyter notebook --ip='*' --NotebookApp.token='' --NotebookApp.password=''
-
+jupyter notebook \
+  --ServerApp.ip=0.0.0.0 \
+  --ServerApp.port=8888 \
+  --ServerApp.open_browser=False \
+  --ServerApp.token='' \
+  --ServerApp.password=''
